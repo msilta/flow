450eb25393e51b4c4b9986ecf25ee8b5a5c9390e
Tero Multisilta <tero.multisilta@gmail.com>
2012-03-06 00:26:37 +0200

    Added tabs

--- apps/flow/english.lproj/strings.js
+++ apps/flow/english.lproj/strings.js
@@ -3,9 +3,6 @@
 // Copyright: @2012 Tero Multisilta
 // ==========================================================================
 
-Flow.main = function main() {
-  document.title = "Flow";
-  Flow.getPath('mainPage.mainPane').append();
-};
-
-function main() { Flow.main(); }
+SC.stringsFor('en',{
+  // I18N
+});
---
+++ apps/flow/views/main_page.js
@@ -0,0 +1,54 @@
+require('views/project_list');
+
+
+Flow.mainPage = SC.Page.create({
+  
+  pageName: 'Flow.mainPage',
+  
+  mainPane: SC.MainPane.design({
+    
+    childViews: [SC.TabView.design({
+      
+      value: 'welcome',
+      
+      items: [
+        { title: "Welcome", value: "welcome" },
+        { title: "Projects", value: "projects" },
+        { title: "Config", value: "config" },
+        { title: "About", value: "about" }
+      ],
+      
+      itemTitleKey: 'title',
+      itemValueKey: 'value',
+            
+      layout: { left:12, right:12, top:12, bottom:12 },
+      
+      userDefaultKey: "mainPane"
+      
+    })]
+    
+  }),
+
+  welcome: SC.LabelView.design({
+    escapeHTML: NO,
+    classNames: 'welcome-tab',
+    value: "<h1>Welcome</h1>"
+  }),
+  
+  projects: SC.TemplateView.create({
+    templateName: 'flow'
+  }),
+  
+  config: SC.LabelView.design({
+    escapeHTML: NO,
+    classNames: 'config-tab',
+    value: "<h1>Config</h1>"
+  }),
+    
+  about: SC.LabelView.design({
+    escapeHTML: NO,
+    classNames: 'about-tab',
+    value: "<h1>About</h1>"
+  })
+  
+});
